c288c42365fc369dc9eccb54636cb962b37218e2
Darshan7573 <darshu7573@gmail.com>
2024-11-15 10:46:31 +0530

    connection done with mongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import connectDB from './config/mongodb.js'
+import 'dotenv/config'
 
 const app = express()
 const port = 5000
+connectDB()
 
 app.use(express.json())
 app.use(cors())
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from server')
