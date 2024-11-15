81d6a3a5992f1718984ca93c576678e4f8d23f2c
Darshan7573 <darshu7573@gmail.com>
2024-11-15 11:42:30 +0530

    login user functionality is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,4 +35,29 @@ const registerUser = async (req, res) => {
     }
 }
 
-export { registerUser }
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const user = await userMondel.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, message: 'Uset does not exist' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+        } else {
+            res.json({
+                success: false, message: 'Invalid Credentials'
+            })
+        }
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { registerUser, loginUser }
